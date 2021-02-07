4650256df0bf99e69076a39d97e70164dbbddfe7
JonathanLoscalzo <jonathan.r.loscalzo@gmail.com>
2021-02-07 01:02:08 -0300

    files from datadriven template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='En esta competencia, analizaremos la actividad de los usuarios que visitan un servicio ofrecido por medio de un sitio web.  El objetivo será predecir que visitantes decidirán comprar de acuerdo a las características e interacciones que ejecuten en la página. Se toman en cuenta datos como Bounce rate, Month, Pages Values, Operating System, Duration, Period Related y muchos más.',
+    author='jonathanloscalzo',
+    license='MIT',
+)
